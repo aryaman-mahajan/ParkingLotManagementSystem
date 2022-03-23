d1cc30bd0087544f7873231f513a727fb0e9a2d9
aryaman-mahajan <100345262+aryaman-mahajan@users.noreply.github.com>
2022-03-23 11:49:10 +0530

    Update ParkingLotSuite.scala

--- ParkingLotSuite.scala
+++ ParkingLotSuite.scala
@@ -1,4 +1,5 @@
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.{be, noException}
 
 class ParkingLotSuite extends org.scalatest.funsuite.AnyFunSuite{
   val platform1 = ParkingLot(1,3,2)
@@ -7,9 +8,9 @@ class ParkingLotSuite extends org.scalatest.funsuite.AnyFunSuite{
     assertThrows[Exception]{val bike2= new TwoWheeler("Bike2",platform1)}
   }
   test("Allows entry of Bike on exit of 1"){
-    assertCompiles{
-      """bike1.exit()
-        |val bike2= new TwoWheeler("Bike2",platform1)""".stripMargin}
+    noException should be thrownBy {
+        bike1.exit();
+        val bike2= new TwoWheeler("Bike2",platform1)}
   }
   val car1= new Hatchback("Car1",platform1)
   val car2= new Hatchback("Car2",platform1)
@@ -18,9 +19,9 @@ class ParkingLotSuite extends org.scalatest.funsuite.AnyFunSuite{
     assertThrows[Exception]{val car4= new Hatchback("Car4",platform1)}
   }
   test("Allows entry of Car on exit of 1"){
-    assertCompiles{
-      """car3.exit()
-        |val car4= new Hatchback("Car4",platform1)""".stripMargin}
+    noException should be thrownBy {
+      car3.exit();
+      val car4= new Hatchback("Car4",platform1)}
   }
   val suv1= new Suv("Suv1",platform1)
   val suv2= new Suv("Suv2",platform1)
@@ -28,9 +29,9 @@ class ParkingLotSuite extends org.scalatest.funsuite.AnyFunSuite{
     assertThrows[Exception]{val suv3= new Suv("Suv3",platform1)}
   }
   test("Allows entry of Suv on exit of 1"){
-    assertCompiles{
-      """suv2.exit()
-        |val suv3= new Suv("Suv3",platform1)""".stripMargin}
+    noException should be thrownBy {
+      suv2.exit();
+      val suv3= new Suv("Suv3",platform1)}
   }
   test("Vehicle Not in Parking Lot"){
     assertResult(println("Vehicle not in Parking Lot")){platform1.getDetails("Car7")}
